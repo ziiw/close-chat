f55dfd71e6c68cacf9d985904e22570068591912
Adrien Lav <adrienl@outlook.com>
2020-04-06 11:17:52 +0200

    Link Stripe + Store in Firebase

--- pages/account.js
+++ pages/account.js
@@ -1,26 +1,25 @@
 import React, { Fragment, useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
 import dynamic from 'next/dynamic'
-import styled from 'styled-components'
 import Title from './../components/elements/h1'
 import firebase from './../lib/firebase'
 
 const Account = props => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState()
 
   useEffect( () => {
     setUser(firebase.auth().currentUser)
   })
 
   const signout = () => firebase.auth().signOut()
-
+  console.log('account', user && user.uid)
   return (
     <Fragment>
-      <Title>Account</Title><br></br>
+      <Title>Account</Title><br />
       {user && <button onClick={signout}>Sign out</button>}
       {user && <p>{`Hello ${user.email}`}</p>}
       {user && user.emailVerified && <p>{`You email is valid`}</p>}
       {user && !user.emailVerified && <p>{`You email is not validated`}</p>}
+      {user && <a href={`https://connect.stripe.com/express/oauth/authorize?redirect_uri=${process.env.STRIPE_REDIRECT_URI}/${user.email}&client_id=ca_H1BEnLhjmtGa2SV3dQWV3KFz3kgg24zx&state=234jkbkbu234bkjui99eff4b&stripe_user[business_type]=individual&stripe_user[email]=${user.email}`}>Connect with Stripe</a>}
     </Fragment>
   )
 } 
---
+++ pages/api/account/stripe/[email].js
@@ -0,0 +1,34 @@
+// Set your secret key. Remember to switch to your live secret key in production!
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+const stripe = require('stripe')(process.env.STRIPE_SECRET_API);
+import admin from './../../../../utils/auth/firebaseAdmin'
+
+export default async (req, res) => {
+  const {
+    query: { email, code },
+  } = req
+
+  console.log(`Verifying account ${email}, with ${code}`)
+
+  try {
+    const response = await stripe.oauth.token({
+      grant_type: 'authorization_code',
+      code,
+    })
+
+    const data = {stripeUserId: response.stripe_user_id}
+
+    const db = admin.firestore()
+    const ref = db.collection(`creators`).doc(email)
+    const snap = await ref.get()
+
+    snap.exists ? await ref.update(data) : await ref.set(data)
+
+    res.writeHead(302, {'Location': `/account`});
+  } catch (error) {
+    console.log(error)
+    res.writeHead(302, {'Location': `/account?error=${error}`})
+  } finally {
+    res.end()
+  }
+}
