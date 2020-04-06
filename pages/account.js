a05601739f4bdefe57ef835d8dd001dfe50e6b21
Adrien Lav <adrienl@outlook.com>
2020-04-06 13:17:06 +0200

    Fix redirect_uri Stripe

--- pages/account.js
+++ pages/account.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useEffect, useState } from 'react'
+import Router from 'next/router'
 import dynamic from 'next/dynamic'
 import Title from './../components/elements/h1'
 import firebase from './../lib/firebase'
@@ -11,7 +12,12 @@ const Account = props => {
   })
 
   const signout = () => firebase.auth().signOut()
-  console.log('account', user && user.uid)
+
+  const api_route = '/api/account/stripe'
+  const host = window.location.host
+  const protocol = host.indexOf('localhost') === -1 ? 'https://' : 'http://'
+  const redirect_uri = `${protocol}${host}${api_route}/${user && user.email}`
+
   return (
     <Fragment>
       <Title>Account</Title><br />
@@ -19,7 +25,7 @@ const Account = props => {
       {user && <p>{`Hello ${user.email}`}</p>}
       {user && user.emailVerified && <p>{`You email is valid`}</p>}
       {user && !user.emailVerified && <p>{`You email is not validated`}</p>}
-      {user && <a href={`https://connect.stripe.com/express/oauth/authorize?redirect_uri=${process.env.STRIPE_REDIRECT_URI}/${user.email}&client_id=ca_H1BEnLhjmtGa2SV3dQWV3KFz3kgg24zx&state=234jkbkbu234bkjui99eff4b&stripe_user[business_type]=individual&stripe_user[email]=${user.email}`}>Connect with Stripe</a>}
+      {user && <a href={`https://connect.stripe.com/express/oauth/authorize?redirect_uri=${redirect_uri}&client_id=ca_H1BEnLhjmtGa2SV3dQWV3KFz3kgg24zx&state=234jkbkbu234bkjui99eff4b&stripe_user[business_type]=individual&stripe_user[email]=${user.email}`}>Connect with Stripe</a>}
     </Fragment>
   )
 } 
